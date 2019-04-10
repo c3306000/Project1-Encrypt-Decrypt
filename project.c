ec9e293d0f18680089f5d35f0240cf43074997b5
Caleb Hampton <caleb.hampton@uon.edu.au>
2019-04-10 00:15:26 +0000

    string initialisation

--- project.c
+++ project.c
@@ -2,12 +2,9 @@
 
 int main()  {
     
-    char A = 65, B = 66, C = 67, D = 68, E = 69, F = 70, G = 71, H = 72, I = 73, J = 74, K = 75, L = 76, M = 77;
-    char N = 78, O = 79, P = 80, Q = 81, R = 82, S = 83, T = 84, U = 85, V = 86, W = 87, X = 88, Y = 89, Z = 90;
-    char text;
+    char code[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
     
-    //scanf("%c", &text);
-    printf("%c\n", A);
+    printf("%s\n", code);
     
     return 0;
 }  
