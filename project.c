afae7c8dbabbab62d7689851b1c78b89f41e94e4
Caleb Hampton <caleb.hampton@uon.edu.au>
2019-04-10 00:20:31 +0000

    string

--- project.c
+++ project.c
@@ -4,7 +4,7 @@ int main()  {
     
     char code[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
     
-    printf("%s\n", code);
+    printf("%s\n", code); // print
     
     return 0;
 }  
