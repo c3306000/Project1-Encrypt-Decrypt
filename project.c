9a1bf66968a77ba128025ec1cbf323bcf05ce751
Caleb Hampton <caleb.hampton@uon.edu.au>
2019-04-26 00:58:36 +0000

    rz1

--- project.c
+++ project.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
-
-int main()  {
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    char word[100];
+    int key;
+    int i = 0;
+    printf("Enter text: \n");
+    scanf("%c", )
+    printf("Enter a key between 0 and 26: /n");
+    scanf("%d", &key);
     
-    char c;
-    scanf("%c", &c);
-    printf("%c", c);
+    for(i = 0; i < strlen(word); i++)
+    {
+        if(word[i] >= 'Z')
+        {
+            word[i] = word[i] - 32;
+        }
+    }
     
+    printf("String in upper case is %s\n", word);
+    for(i = 0; word[i] != '\0'; i++)
+    {
+        word[i] = word[i] + key;
+    }
+    for(i = 0; i < strlen; i++)
+    {
+        word[i] = word[i] -26;
+    }
+    
+    printf("Encoded message is: %s", word);
     return 0;
-}  
+}
